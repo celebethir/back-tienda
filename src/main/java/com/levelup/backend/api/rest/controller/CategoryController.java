3fd78101a702221efa40e2de958942fc09c07b00
ddelpino <daniel.delpino@edataconsulting.de>
2025-03-19 08:16:59 +0000

    Added category endpoints

--- src/main/java/com/levelup/backend/api/rest/controller/CategoryController.java
+++ src/main/java/com/levelup/backend/api/rest/controller/CategoryController.java
@@ -4,9 +4,7 @@ import com.levelup.backend.api.model.dto.CategoryDto;
 import com.levelup.backend.service.category.CategoryService;
 import com.levelup.backend.service.converter.category.CategoryProjectionToDtoConverter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +24,26 @@ public class CategoryController {
 
     @GetMapping
     public List<CategoryDto> getCategories() {
-        return categoryService.getAll().stream().map(CategoryProjectionToDtoConverter::convert);
+        return categoryService.getAll().stream().map(categoryProjectionToDtoConverter::convert).toList();
+    }
+
+    @GetMapping("/{id}")
+    public CategoryDto get(@PathVariable final Long id) {
+        return categoryProjectionToDtoConverter.convert(categoryService.findById(id));
+    }
+
+    @PostMapping
+    public CategoryDto create(@RequestBody final CategoryDto categoryDto) {
+        return categoryProjectionToDtoConverter.convert(categoryService.createCategory(categoryDto));
+    }
+
+    @PutMapping("/{id}")
+    public CategoryDto update(@PathVariable final Long id, final @RequestBody CategoryDto categoryDto) {
+        return categoryProjectionToDtoConverter.convert(categoryService.updateCategory(id, categoryDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable final Long id) {
+        categoryService.deleteCategory(id);
     }
 }
